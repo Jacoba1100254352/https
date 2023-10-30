460f766b1aa413e1bf281d01c55a64a932ffabef
Jacob Anderson <jacobdanderson@gmail.com>
2023-10-29 22:07:16 -0600

    General code format

--- http_server.py
+++ http_server.py
@@ -1,75 +1,110 @@
-import logging, random, socket, argparse, struct, sys
+import argparse, logging, socket, sys, time, os
 
 
-def randomize_text(text):
+def setup_logging(verbose):
     """
-    Randomize text by repeating characters and discarding some of them.
+    Sets up the logging based on verbosity level.
 
-    :param text: The input text to be randomized.
-    :return: A randomized version of the input text.
+    @param verbose: Whether to enable verbose logging.
+    @return: None
     """
-
-    def discard():
-        return random.choices([True, False], weights=[1, 5])[0]
-
-    def repeat(char):
-        should_repeat = random.choices([True, False], weights=[1, 5])[0]
-        return char * (int(random.paretovariate(1)) if should_repeat else 1)
-
-    transformed_text = [repeat(c) for c in text if not discard()]
-    return "".join(transformed_text) if transformed_text else text[0]
+    level = logging.DEBUG if verbose else logging.INFO
+    logging.basicConfig(format="%(levelname)s:%(message)s", level=level)
 
 
-def parse_header(header):
+def send_response(conn, file_path, header, root_folder):
     """
-    Parse the binary header to extract action and message length.
+    Sends server response to the client.
 
-    :param header: The binary header to be parsed.
-    :return: A tuple containing the action and message length.
+    @param conn: The client connection socket.
+    @param file_path: The path to the file to be sent.
+    @param header: The HTTP header for the response.
+    @param root_folder: The root folder for server files.
+    @return: None
     """
-    # Variables for decoding header
-    binary_header = format(header, "032b")
-    action_code = int(binary_header[:5], 2)
-    message_length = int(binary_header[5:], 2)
+    send_num = 0
+    full_path = root_folder + file_path
+    try:
+        file_size = os.path.getsize(full_path)
+    except FileNotFoundError:
+        file_size = os.path.getsize(root_folder + "/404.html")
+        full_path = root_folder + "/404.html"
+    response_header = header + f"Content-Length: {file_size}\r\n\r\n"
+    conn.sendall(response_header.encode())
 
-    # Log
-    logging.debug(
-        f"Header: {header}, Binary Header: {binary_header}, actionCode: {action_code}, Message Length: {message_length}"
-    )
+    with open(full_path, "rb") as f:
+        while True:
+            file_content = f.read(1024)
+            if not file_content:
+                break
+            conn.sendall(file_content)
+            send_num += 1
+            logging.info(f"Send {send_num}")
 
-    # Define and return actions and info
-    actions = {1: "uppercase", 2: "lowercase", 4: "reverse", 8: "shuffle", 16: "random"}
-    return actions.get(action_code, "error"), message_length
 
-
-def process_data(action, data):
+def get_header(code):
     """
-    Process the data based on the given action.
+    Gets the response header based on HTTP response code.
 
-    :param action: The action to be applied to the data.
-    :param data: The input data to be processed.
-    :return: The processed data based on the specified action.
+    @param code: The HTTP response code.
+    @return: The response header as a string.
     """
-    if action == "uppercase":
-        return data.upper()
-    elif action == "lowercase":
-        return data.lower()
-    elif action == "reverse":
-        return data[::-1]
-    elif action == "shuffle":
-        return "".join(random.sample(data, len(data)))
-    elif action == "random":
-        return randomize_text(data)
+    if code == 200:
+        return "HTTP/1.1 200 OK\r\n"
+    elif code == 404:
+        return "HTTP/1.1 404 Not Found\r\n"
     else:
-        return "error"
+        return "HTTP/1.1 405 Method Not Allowed \r\n"
 
 
-def run(port):
+def is_request_file_exist(request_file, root_folder):
     """
-    Run the server.
+    Checks if a requested file exists.
 
-    :param port: The port number on which the server listens.
-    :return: None
+    @param request_file: The path to the requested file.
+    @param root_folder: The root folder for server files.
+    @return: True if the file exists, False otherwise.
+    """
+    return os.path.isfile(root_folder + request_file)
+
+
+def process_request(conn, request, root_folder):
+    """
+    Processes the client request and sends the appropriate response.
+
+    @param conn: The client connection socket.
+    @param request: The client HTTP request as a string.
+    @param root_folder: The root folder for server files.
+    @return: None
+    """
+    beg_line = request.split("\r\n")[0]
+    info = beg_line.split(" ")
+    method = info[0]
+    request_file = info[1]
+
+    if method != "GET":
+        send_response(conn, None, get_header(405), root_folder)
+        return
+
+    if request_file == "/":
+        send_response(conn, "/page.html", get_header(200), root_folder)
+        return
+
+    if not is_request_file_exist(request_file, root_folder):
+        send_response(conn, "/404.html", get_header(404), root_folder)
+        return
+
+    send_response(conn, request_file, get_header(200), root_folder)
+
+
+def run(port, root_folder, delay):
+    """
+    Runs the HTTP server and listens for incoming client requests.
+
+    @param port: The port number to listen on.
+    @param root_folder: The root folder for server files.
+    @param delay: Whether to introduce a delay before processing requests (for debugging).
+    @return: None
     """
     server_socket = socket.socket()
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -82,6 +117,9 @@ def run(port):
             conn, address = server_socket.accept()
             logging.info(f"Connection from: {address}")
 
+            if delay:
+                time.sleep(5)
+
             data_buffer = bytearray()
             while True:
                 chunk = conn.recv(1024)
@@ -89,19 +127,11 @@ def run(port):
                     break
                 data_buffer.extend(chunk)
 
-                while len(data_buffer) >= 4:
-                    header = struct.unpack("!I", data_buffer[:4])[0]
-                    action, msg_len = parse_header(header)
+                data_str = data_buffer.decode()
 
-                    if len(data_buffer) < 4 + msg_len:
-                        break
-
-                    message = data_buffer[4 : 4 + msg_len].decode("utf-8", "ignore")
-                    logging.info(f"Received: {message}")
-                    response = process_data(action, message)
-                    conn.send(struct.pack("!I", len(response)) + response.encode())
-
-                    data_buffer = data_buffer[4 + msg_len :]
+                if "\r\n\r\n" in data_str:
+                    process_request(conn, data_str, root_folder)
+                    break
 
             conn.close()
             logging.info(f"Connection closed")
@@ -110,29 +140,20 @@ def run(port):
         sys.exit(0)
 
 
-def setup_logging(verbose):
-    """
-    Set up logging based on verbosity.
-
-    :param verbose: A boolean indicating whether to enable verbose logging.
-    :return: None
-    """
-    level = logging.DEBUG if verbose else logging.INFO
-    logging.basicConfig(format="%(levelname)s:%(message)s", level=level)
-
-
 if __name__ == "__main__":
     # Parse arguments
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Simple HTTP Server")
     parser.add_argument(
-        "-p", "--port", required=False, type=int, default=8083, help="port to bind to"
+        "-p", "--port", required=False, type=int, default=8084, help="port to bind to"
     )
     parser.add_argument(
-        "-v",
-        "--verbose",
-        required=False,
-        action="store_true",
-        help="turn on debugging output",
+        "-v", "--verbose", required=False, action="store_true", help="turn on debugging output"
+    )
+    parser.add_argument(
+        "-d", "--delay", required=False, action="store_true", help="add a delay for debugging purposes"
+    )
+    parser.add_argument(
+        "-f", "--folder", required=False, default=".", help="folder from where to serve from"
     )
     args = parser.parse_args()
 
@@ -140,5 +161,9 @@ if __name__ == "__main__":
     setup_logging(args.verbose)
     logging.debug(args)
 
+    # Ensure the root folder ends with a '/'
+    if args.folder[-1] != "/":
+        args.folder += "/"
+
     # Run the code
-    run(args.port)
+    run(args.port, args.folder, args.delay)
